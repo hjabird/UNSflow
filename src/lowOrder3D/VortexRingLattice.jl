c350fdbf7695d7ff686ea14e010fb721d235cd76
HJA Bird <9040797+hjabird@users.noreply.github.com>
2018-10-31 15:54:38 +0000

    Add add_row!, print, fix for zero size lattice evaluation

--- src/lowOrder3D/VortexRingLattice.jl
+++ src/lowOrder3D/VortexRingLattice.jl
@@ -30,7 +30,7 @@ mutable struct VortexRingLattice <: Vorticity3D
     vorticity :: Matrix{Float64}
     geometry :: BilinearQuadSurf
 
-    function VortexRingLattice(x_rings::Float64, y_rings::Float64)
+    function VortexRingLattice(x_rings::T, y_rings::S) where {T<:Int, S<:Int}
         strs = zeros(x_rings, y_rings)
         geometry = BilinearQuadSurf(x_rings, y_rings)
         new(strs, geometry)
@@ -78,6 +78,9 @@ end
 
 function induced_velocity(a::VortexRingLattice, measurement_loc::Vector3D)
     vel = Vector3D(0,0,0)
+    if length(a.vorticity) == 0
+        return vel
+    end
     id, jd = size(a.vorticity)
     c = a.geometry.coordinates
     # internal horizontal filaments
@@ -123,6 +126,9 @@ end
 
 function induced_velocity_curl(a::VortexRingLattice)
     curl = zeros(3,3)
+    if length(a.vorticity) == 0
+        return curl
+    end
     id, jd = size(a.vorticity)
     # internal horizontal filaments
     for i = 2 : id
@@ -275,7 +281,7 @@ function unsteady_forces(
     m_areas = vec(areas(a.geometry))
     m_normals = vec(normals(a.geometry))
     m_forces = map(x->x[1]*unit(x[2])*x[3], 
-        zip(m_unsteady_pressures, m_unsteady_normals, m_areas))
+        zip(m_unsteady_pressures, m_normals, m_areas))
     m_forces = reshape(m_forces, size(a.vorticity))
     return m_forces
 end
@@ -384,11 +390,11 @@ function update_using_state_vector!(
     this::VortexRingLattice,
     state_vect::Vector{Float64})
 
-    n = length(this.ring_vertices)
-    @assert(length(state_vector) == n * 3, string(
+    n = length(coords(this.geometry))
+    @assert(length(state_vect) == n * 3, string(
         "State vector was incorrect length. Length was ", length(state_vect),
         " but should have been ", n * 3, "."))
-    if any(isnan.(state_vect) || isinf.(state_vect))
+    if any(isnan.(state_vect)) || any(isinf.(state_vect))
         @warn "Infinite or NaN values in state vector."
     end
     for i = 1 :  n
@@ -403,9 +409,10 @@ function state_time_derivative(
     inducing_bodies::Vorticity3D)
 
     svtd = Vector{Float64}(undef, 3 * length(this.geometry.coordinates))
-    for i = 1 : length(this.ring_vertices)
-        svtd[i*3 - 2 : i*3] = convert(Vector{Float64},
-             induced_velocity(this, inducing_bodies))
+    c = coords(this.geometry)
+    for i = 1 : length(c)
+        svtd[i*3 - 2 : i*3] = 
+            convert(Vector{Float64}, induced_velocity(inducing_bodies, c[i]))
     end
     return svtd
 end
@@ -415,7 +422,9 @@ function vorticity_vector_length(this::VortexRingLattice)
 end
 
 function vorticity_vector(this::VortexRingLattice)
-    return vec(this.vorticity)
+    return deepcopy(vec(this.vorticity))
+    # Otherwise updating this.vorticity will pdate the output vorticity_vector
+    # which is no use if you were trying to store the old one for example.
 end
 
 function update_using_vorticity_vector!(
@@ -468,6 +477,20 @@ function extract_vorticity_vector_indexes(
     lookup = [ni * (j-1) + i for i = 1 : ni, j = 1 : nj]
     ret = map(x->lookup[x[1], x[2]], [(i, j) for i in i_ind, j in j_ind])
     return ret
+end
+
+function add_row!(
+    a::VortexRingLattice, 
+    points::Vector{Vector3D},
+    strengths::Vector{T}=zeros(1, size(a.coords, 2) - 1)) where T <: Real
+    coords = a.geometry.coordinates
+    @assert(size(coords, 2) == length(points), string(
+        "Dimension mismatch: size(VortRingLattice.geometry.coords, 2) must ",
+        "match length(points). length(points) = ", length(points), " and ",
+        " size(VortRingLattice.geometry.coords, 2) = ", size(coords, 2)))
+    a.geometry.coordinates = vcat(coords, reshape(points, 1, :))
+    a.vorticity = vcat(a.vorticity, reshape(strengths, 1, :))
+    return
 end
 
 #= CONVERSION FUNCTIONS ------------------------------------------------------=#
@@ -611,4 +634,12 @@ function add_pointdata!(a::MeshDataLinker, b::VortexRingLattice,
         add_pointdata!(a, dataname, v[2], v[1])
     end
     return
+end
+
+function Base.print(a::VortexRingLattice)
+    print(typeof(a), " with size ", size(a.geometry), ". ")
+end
+
+function Base.println(a::VortexRingLattice)
+    print(typeof(a), " with size ", size(a.geometry), ".\n ")
 end
