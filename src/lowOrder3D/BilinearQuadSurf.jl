0a11dc3055384aa10fd247af278d3e7ef611ee89
HJA Bird <9040797+hjabird@users.noreply.github.com>
2018-11-02 13:38:03 +0000

    Variable name change to reflect local coordinates

--- src/lowOrder3D/BilinearQuadSurf.jl
+++ src/lowOrder3D/BilinearQuadSurf.jl
@@ -92,16 +92,16 @@ function Base.length(a::BilinearQuadSurf)
 end
 
 function Base.convert(::Type{BilinearQuad}, a::BilinearQuadSurf,
-    xidx::Int, yidx::Int)
+    iidx::Int, jidx::Int)
 
-    @assert(xidx < size(a.coordinates, 1), string("xidx of requested Bilinear",
-        "Quad was too high. Requested x index of ", xidx, 
+    @assert(iidx < size(a.coordinates, 1), string("iidx of requested Bilinear",
+        "Quad was too high. Requested i index of ", iidx, 
         " which should have been less than ", size(a.coordinates, 1), "."))
-    @assert(yidx < size(a.coordinates, 2), string("yidx of requested Bilinear",
-        "Quad was too high. Requested y index of ", yidx, 
+    @assert(jidx < size(a.coordinates, 2), string("jidx of requested Bilinear",
+        "Quad was too high. Requested j index of ", jidx, 
         " which should have been less than ", size(a.coordinates, 2), "."))
     v = a.coordinates
-    g = [v[xidx+1, yidx], v[xidx+1, yidx+1], v[xidx, yidx+1], v[xidx, yidx]]
+    g = [v[iidx+1, jidx], v[iidx+1, jidx+1], v[iidx, jidx+1], v[iidx, jidx]]
     return BilinearQuad(g)
 end
 
